e2e6f9c3e7d4d802fd878ff452e003a71b934cda
Tim Kaiser <kaisertimothy@johndeere.com>
2020-11-09 20:14:34 -0600

    removed class from mav component

--- but-its-mine/src/components/Navigation.js
+++ but-its-mine/src/components/Navigation.js
@@ -1,53 +1,37 @@
-import React, {Component} from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
-class Navigation extends Component {
-    constructor(props) {
-        super(props)
-    
-        this.state = {
-        //   activePage: null
-        }
-      }
-    
-    // setActivePage = (activePage) => {
-    //     this.setState({
-    //         activePage
-    //     })
-    // }
-
-    render() {
-        return (
-            <div className = 'nav'>
-                <h3>Navigation</h3>
-                <Link 
-                    to = '/' 
-                    onClick = {()=>this.props.setActivePage('homepage')}
-                    className = {this.props.activePage === 'homepage' ? "active" : null}
-                >Homepage</Link>
-                <Link 
-                    to = '/start' 
-                    onClick = {()=>this.props.setActivePage('start')}
-                    className = {this.props.activePage === 'start' ? "active" : null}
-                >Before you start</Link>
-                <Link 
-                    to = '/planning' 
-                    onClick = {()=>this.props.setActivePage('planning')}
-                    className = {this.props.activePage === 'planning' ? "active" : null}
-                >Planning</Link>
-                <Link 
-                    to = '/build' 
-                    onClick = {()=>this.props.setActivePage('build')}
-                    className = {this.props.activePage === "build" ? "active" : null}
-                >Build</Link>
-                <Link 
-                    to = '/forum' 
-                    onClick = {()=>this.props.setActivePage('forum')}
-                    className = {this.props.activePage === 'forum' ? "active" : null}
-                >Forum</Link>
-            </div>
-        )
-    }
+function Navigation(props) {
+    return (
+        <div className = 'nav'>
+            <h3>Navigation</h3>
+            <Link 
+                to = '/' 
+                onClick = {()=>props.setActivePage('homepage')}
+                className = {props.activePage === 'homepage' ? "active" : null}
+            >Homepage</Link>
+            <Link 
+                to = '/start' 
+                onClick = {()=>props.setActivePage('start')}
+                className = {props.activePage === 'start' ? "active" : null}
+            >Before you start</Link>
+            <Link 
+                to = '/planning' 
+                onClick = {()=>props.setActivePage('planning')}
+                className = {props.activePage === 'planning' ? "active" : null}
+            >Planning</Link>
+            <Link 
+                to = '/build' 
+                onClick = {()=>props.setActivePage('build')}
+                className = {props.activePage === "build" ? "active" : null}
+            >Build</Link>
+            <Link 
+                to = '/forum' 
+                onClick = {()=>props.setActivePage('forum')}
+                className = {props.activePage === 'forum' ? "active" : null}
+            >Forum</Link>
+        </div>
+    )
 }
 
 export default Navigation;
