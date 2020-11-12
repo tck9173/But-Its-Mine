a52f5507c61f7837c520d4d37fb514651b8df32c
Tim Kaiser <kaisertimothy@johndeere.com>
2020-11-12 16:43:26 -0600

    reverted to local

--- but-its-mine/src/services/api_helper.js
+++ but-its-mine/src/services/api_helper.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL : 'https://sleepy-brushlands-19849.herokuapp.com/'
+    //baseURL : 'https://sleepy-brushlands-19849.herokuapp.com'
+    baseURL : 'http://localhost:3001'
 })
 
 export const registerUser = async (registerData) =>{
