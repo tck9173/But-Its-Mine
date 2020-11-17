0a1f476c216991e06cb80baf54997b5aae827e2b
Tim Kaiser <kaisertimothy@johndeere.com>
2020-11-16 20:36:22 -0600

    added mobile styling

--- but-its-mine/src/components/Homepage/FeaturedBenchmark.js
+++ but-its-mine/src/components/Homepage/FeaturedBenchmark.js
@@ -5,7 +5,7 @@ function FeaturedBenchmark() {
         <div className='featured'>
             <h2>This week's featured benchmark</h2>
             <h3>Heaven Benchmark from Unigine</h3>
-            <iframe title='Featured Benchmark' width="874" height="500" src="https://www.youtube.com/embed/uEXv8A0-_KU" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe title='Featured Benchmark' src="https://www.youtube.com/embed/uEXv8A0-_KU" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             <p>This is a great benchmark for anyone building a system for the first time. Go check it out!</p>
             <a href='https://benchmark.unigine.com/heaven' target="_blank" rel="noreferrer">Heaven Benchmark by Unigine</a>
         </div>
